dd88ab457be905b1758d3888e8719fbc217fc1ed
Vadym Pyntia <Vadym_Pyntia@epam.com>
2020-07-15 00:13:35 +0300

    3 authentication types implemented.

--- GitHubAPI/GitHubHttpClient.cs
+++ GitHubAPI/GitHubHttpClient.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 
 namespace GitHubAPI
 {
     public class GitHubHttpClient : HttpClient
     {
-        static string gitHubUri = "https://api.github.com";
+        public static string GitHubUri { get; } = "https://api.github.com";
 
-        public GitHubHttpClient()
+        public GitHubHttpClient(System.Net.Http.Headers.AuthenticationHeaderValue authentication)
         {
             DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             DefaultRequestHeaders.AcceptCharset.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("utf-8"));
-            DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue(new System.Net.Http.Headers.ProductHeaderValue("pyntya")));
-            BaseAddress = new Uri(gitHubUri);
+            DefaultRequestHeaders.Authorization = authentication;
+            DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue(new System.Net.Http.Headers.ProductHeaderValue(AppDomain.CurrentDomain.FriendlyName)));
+            BaseAddress = new Uri(GitHubUri);
         }
     }
 }
--- GitHubAPI/Program.cs
+++ GitHubAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GitHubAPI.Extensions;
+using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace GitHubAPI
@@ -7,13 +9,52 @@ namespace GitHubAPI
     {
         static void Main(string[] args)
         {
-            var dictionary = GitHubApi.GetApiList();
-            var categories = dictionary.Select(x => x.Value.Split("/")[3]).GroupBy(x => x).OrderByDescending(x => x.Count());
-            var queries = dictionary.Where(x => x.Value.Contains("{query}"));
+            System.Net.Http.Headers.AuthenticationHeaderValue authentication;
 
-            Console.WriteLine($"1. Number of endpoints: {dictionary.Count}");
-            Console.WriteLine($"2. {string.Join(", ", categories.Select(x => $"{x.Key}: {x.Count()}"))}");
-            Console.WriteLine($"3. {string.Join(Environment.NewLine, queries.Select(x => x.Value))}");
+            if (args.Length == 1 && args[0] == "-h")
+            {
+                WriteHelpMessage();
+                return;
+            }
+            else if (args.Length == 0)
+                authentication = null;
+            else if (args.Length == 1 && Enum.TryParse(typeof(AuthenticationTypes), args[0], true, out var authType) && (AuthenticationTypes)authType == AuthenticationTypes.NoAuthentication)
+                authentication = null;
+
+            else if (args.Length == 2 && Enum.TryParse(typeof(AuthenticationTypes), args[0], true, out var authenticationType))
+            {
+                authentication = (AuthenticationTypes)authenticationType switch
+                {
+                    AuthenticationTypes.Basic => new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", args[1].GetBase64EncodedString()),
+                    AuthenticationTypes.OAuth2 => new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", args[1]),
+                    _ => null
+                };
+            }
+            else
+            {
+                Console.WriteLine(ErrorMessage);
+                return;
+            }
+
+            var context = new GitHubApiContext(authentication);
+
+            Console.WriteLine(GetNumberOfEndpointsReultString(context));
+            Console.WriteLine(GetCategoriesOrderedByCountDescendingResultString(context));
+            Console.WriteLine(GetQueriesResultString(context));
+        }
+
+        private static string GetNumberOfEndpointsReultString(GitHubApiContext context) => $"1. Number of endpoints: {context.GetEndpointsCount()}";
+        private static string GetCategoriesOrderedByCountDescendingResultString(GitHubApiContext context) => $"2. {string.Join(", ", context.GetGroupedCategoriesOrderedByCountDescending().Select(x => $"{x.Key}: {x.Count()}"))}";
+        private static string GetQueriesResultString(GitHubApiContext context) => $"3. {string.Join(Environment.NewLine, context.GetQueries().Select(x => x.Value))}";
+        private static string ErrorMessage => $"Wrong parameters. {HelpMessage}";
+        private static string HelpMessage => "Possible parameter values:" + Environment.NewLine
+            + "* no parameters for calling API without authentication" + Environment.NewLine
+            + "* \"NoAuthentication\" parameter for calling API without authentication" + Environment.NewLine
+            + "* \"Basic\" parameter followed by credentials in format \"user:password\" for calling API using 'Basic' authentication" + Environment.NewLine
+            + "* \"OAuth2\" parameter followed by \"OAuth token\" for calling API using 'OAuth2' authentication";
+        private static void WriteHelpMessage()
+        {
+            Console.WriteLine(HelpMessage);
         }
     }
 }
